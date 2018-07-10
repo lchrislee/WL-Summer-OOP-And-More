f68a3a0ef49c19b6050cbbee8c10dc40ca7b4768
Winnie Ly <wsly@wpi.edu>
2018-07-10 19:53:36 -0400

    Revised Tax Array Program

--- simple-practice/src/TaxArray.java
+++ simple-practice/src/TaxArray.java
@@ -3,29 +3,29 @@ public class TaxArray {
     public static void main(String args[]) {
         Scanner console = new Scanner(System.in);
         double taxAmount;
+        int [] taxBracket = {0,9526,38701,82501,157501,200001,500001};
         System.out.println("What is your annual income? ");
-        double [] income = new double [1];
-        income [0] = console.nextInt();
-        if (income[0] > 0 && income [0] < 9526) {
-            taxAmount = income [0] * 0.10;
+        double income = console.nextDouble();
+        if (income > taxBracket[0] && income < taxBracket[1]) {
+            taxAmount = income * 0.10;
             System.out.println("Amount of Tax: " + taxAmount);
-        } else if (income[0] > 9526 && income[0] < 38701) {
-            taxAmount = (((income[0] - 9525) * 0.12) + 952.5);
+        } else if (income > taxBracket[1] && income < taxBracket[2]) {
+            taxAmount = (((income - 9525) * 0.12) + 952.5);
             System.out.println("Amount of Tax: " + taxAmount);
-        } else if (income[0] > 38701 && income[0] < 82501) {
-            taxAmount = (((income[0] - 38700) * 0.22) + 4453);
+        } else if (income > taxBracket[2] && income < taxBracket[3]) {
+            taxAmount = (((income - 38700) * 0.22) + 4453);
             System.out.println("Amount of Tax: " + taxAmount);
-        } else if (income[0] > 82501 && income[0]< 157501) {
-            taxAmount = (((income[0]-82500) * 0.24) + 14089.5);
+        } else if (income > taxBracket[3] && income < taxBracket[4]) {
+            taxAmount = (((income-82500) * 0.24) + 14089.5);
             System.out.println("Amount of Tax: " + taxAmount);
-        } else if (income [0] > 157501 && income[0] < 200001) {
-            taxAmount = (((income[0]-157500) * 0.32) + 32089.5);
+        } else if (income > taxBracket[4] && income < taxBracket[5]) {
+            taxAmount = (((income-157500) * 0.32) + 32089.5);
             System.out.println("Amount of Tax: " + taxAmount);
-        } else if (income[0] > 200001 && income[0]< 500001) {
-            taxAmount = (((income[0]-200000) * 0.35) + 45689.5);
+        } else if (income > taxBracket[5] && income < taxBracket[6]) {
+            taxAmount = (((income-200000) * 0.35) + 45689.5);
             System.out.println("Amount of Tax: " + taxAmount);
         } else {
-            taxAmount = (((income[0]-500000) * 0.37) + 150689.5);
+            taxAmount = (((income-500000) * 0.37) + 150689.5);
             System.out.println("Amount of Tax: " + taxAmount);
         }
     }
