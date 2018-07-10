1a2fec8468f260b614e4e200e7493ae6b043c00d
Chris Lee <c.lee.840@gmail.com>
2018-07-10 19:54:50 -0400

    Initial Fish class for fish practice program.

---
+++ simple-practice/02-objects/SimpleFish/src/Fish.java
@@ -0,0 +1,16 @@
+public class Fish
+{
+	public static final int MOUTH_TYPE_WIDE = 0;
+	public static final int MOUTH_TYPE_SMALL = 1;
+	public static final int SCALE_TYPE_ROUND = 0;
+	public static final int SCALE_TYPE_POINTED = 1;
+	public static final int SCALE_TYPE_SKIN = 2;
+
+	private String mName;
+	private double mWeight;
+	private double mLength;
+	private int mAge;
+	private int mMouthType;
+	private int mScaleType;
+	private boolean mIsAlive;
+}
