fd6b74ecdcf6f61fbf8828b9f105654e5f88a855
Winnie Ly <wsly@wpi.edu>
2018-07-23 20:48:08 -0400

    NumInArray (revised)

--- simple-practice/src/NumInArray.java
+++ simple-practice/src/NumInArray.java
@@ -1,41 +1,53 @@
 import java.util.*;
+
+/**
+ * 16) Fill an array of 10 integers with integer values. Prompt the user for input.
+ * Print whether or not their input number is in the array.
+ */
+
 public class NumInArray {
+
+    public static final int ARRAY_LENGTH = 10;
+
     public static void main(String args[]) {
+
         Scanner console = new Scanner(System.in);
-        int [] list = {1,2,3,4,5,6,7,8,9,0};
-        int [] input = new int[10];
-        System.out.println("This array holds the system's list of numbers.");
-        System.out.print("Number: ");
+        int [] list = new int [ARRAY_LENGTH];
+
+        System.out.println("Please enter in 10 numbers: ");
         for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i] + " ");
+            System.out.print("Number " + (i + 1) + ": ");
+            list [i] = console.nextInt();
         }
-        System.out.println();
-        System.out.println("This array holds the user's inputted numbers.");
-        for (int i =0; i < input.length; i++) {
-            System.out.print("Number: " );
-            input[i] = console.nextInt();
+
+        System.out.print("Please enter in another number: ");
+        int userInputNumber = console.nextInt();
+
+        System.out.println("Is this number in the list of inputs you entered before? (yes/no)");
+        String response = console.next();
+
+        if (response.equals("yes") || response.equals("y")) {
+            findLocation(list,userInputNumber);
+        } else {
+            System.out.println("Have a nice day!");
         }
-        System.out.println();
-        System.out.println("Let's see which inputs are in the system's array.");
-        System.out.println();
-        for (int j = 0; j < input.length; j++) {
-            for (int i = 0; i < list.length; i++) {
-                if (Arrays.equals(list,input)) {
-                    System.out.println("Both arrays have the same values.");
-                    break;
-                }
-                if (input[j] == list[i]) {
-                    int same = list[i];
-                    System.out.println(input[j] + " is in both arrays");
-                    System.out.println();
-                    for (int n: input) {
-                        if(n != same) {
-                            System.out.println(n + " is not in the system's array.");
-                        }
-                    }
-                    System.out.println();
-                }
+    }
+
+    /**
+     * Takes the userInputNumber and compares it with the array of inputted numbers to
+     * see where the number appears in the list.
+     *
+     * @param list The array the user inputted integers in
+     * @param target The number the user inputs that is outside of the array
+     */
+
+    public static void findLocation(int [] list, int target) {
+        for (int i = 0; i < list.length; i++) {
+
+            if (list[i] == target) {
+                System.out.println(target + " is found in position " + (i + 1) + " in the list.");
             }
         }
     }
+
 }
